c3ca33e1a2544d6ce4a385940defd0816bdf3d1e
yomarhub <omar.yassine@ynov.com>
2026-01-27 10:21:22 +0100

    refactor: update home page
    Update index page with improved layout and structure

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center p-10">
     <img class="block w-2/3 my-5 mx-auto" src="/logo.png" alt="Logo" />
-    <h1>Welcome to the Home Page</h1>
+    <h1>{{ $t('index.welcome') }}</h1>
     <p>This is the main landing page of the application.</p>
     <NextPage />
   </div>
