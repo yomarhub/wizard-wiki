8633f13e97c6f1e1cb368491e648a95255b54871
yomarhub <omar.yassine@ynov.com>
2026-01-27 10:19:44 +0100

    feat: add articles listing page
    Create articles page
    - Display list of articles from static data
    - Uses ArticleBase component
    - Includes NextPage navigation

---
+++ app/pages/articles.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col p-10">
+    <NextPage />
+    <ul>
+      <ArticleBase v-for="article in articles" :key="article.id" :article="article" />
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import articles from '~/datas/articles'
+</script>
